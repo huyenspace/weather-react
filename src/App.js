4a68dd5f31699a8c0960902d8831f880912ace50
nlttuyen <95175915+nlttuyen@users.noreply.github.com>
2021-12-06 22:00:05 +0700

    update credit link

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="London" />
+        <Weather defaultCity="Paris" />
       </div>
+      <p>
+        This project was coded by{" "}
+        <a href="https://github.com/huyenspace/" target="_blank">
+          Huyen Nguyen
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/huyenspace/weather-react" target="_blank">
+          open-sourced on Github
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://www.netlify.com/" target="_blank">
+          Netlify
+        </a>{" "}
+      </p>
     </div>
   );
 }
